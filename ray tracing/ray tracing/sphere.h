64be8f494f5f3810fa1c4d51f8858c8aaccdbd87
Janarci <86641308+Janarci@users.noreply.github.com>
2023-03-03 14:53:59 +0800

    glass checkpoint

--- ray tracing/ray tracing/sphere.h
+++ ray tracing/ray tracing/sphere.h
@@ -47,7 +47,7 @@ bool sphere::hit(const ray& r, double t_min, double t_max, hit_record& rec) cons
     rec.set_face_normal(r, outward_normal);
     rec.mat_ptr = mat_ptr;
 
-    rec.normal = (rec.p - center) / radius;
+    //rec.normal = (rec.p - center) / radius;
 
     return true;
 }
