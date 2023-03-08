e1023c7b5c1f7b12e62629f2e1dbf6b97de4faa8
Janarci <86641308+Janarci@users.noreply.github.com>
2023-03-08 08:32:54 +0800

    Update main.cpp

--- ray tracing/ray tracing/main.cpp
+++ ray tracing/ray tracing/main.cpp
@@ -165,7 +165,7 @@ int main()
                 pixel_color = pixel_color +  ray_color(r, world, max_depth);
             }
             rtImage->setPixel(i, j, pixel_color.x(), pixel_color.y(), pixel_color.z(), samples_per_pixel);
-            colorUtils::write_color(std::cout, pixel_color, samples_per_pixel);
+            //colorUtils::write_color(std::cout, pixel_color, samples_per_pixel);
         }
     }
     rtImage->saveImage(filename);
